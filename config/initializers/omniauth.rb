430bc19c2d57e6da98e47eceb70142844da881e7
sidewinder2020 <sdwinder1@gmail.com>
2017-11-02 09:59:35 -0600

    Oauth implemented - fixing bugs

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+ provider :google_oauth2, "#{ENV['client_id']}", "#{ENV['client_secret']}"
+end
