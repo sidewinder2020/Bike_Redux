fe1de08ea15848e2b0550a8aa90def81910c7aab
sidewinder2020 <sdwinder1@gmail.com>
2017-11-03 05:43:50 -0600

    created requests route and controller for clients to submit proposals

---
+++ app/controllers/requests_controller.rb
@@ -0,0 +1,10 @@
+class RequestsController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
    resources :sessions, only: [:create, :destroy]
    resources :home, only: [:show]
+   resources :requests
 
    namespace :api do
      namespace :v1 do
