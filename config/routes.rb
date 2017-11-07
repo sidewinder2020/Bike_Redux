bc5a83054081d1423a8d8a611f0bb47f7c42baba
sidewinder2020 <sdwinder1@gmail.com>
2017-11-06 17:24:01 -0700

    removed sqlite3 from gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
    get 'auth/:provider/callback', to: 'sessions#create'
    get 'auth/failure', to: redirect('/')
    get 'signout', to: 'sessions#destroy', as: 'signout'
@@ -36,6 +38,4 @@ Rails.application.routes.draw do
 
      end
    end
-
-   root to: "home#show"
 end
